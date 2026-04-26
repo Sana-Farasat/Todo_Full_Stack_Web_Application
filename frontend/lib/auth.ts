f027a445fbac9dac6b170e801d0f7bd3ad2ef993
Sana Farasat <162926436+Sana-Farasat@users.noreply.github.com>
2026-04-27 02:22:53 +0500

    added backend url

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 import { jwtClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "https://sana-naz-todo-full-stack-application.hf.space",
   plugins: [jwtClient()],
 });
 
